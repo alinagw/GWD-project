c5d51faf3a660ab5f9b7e1fe2f4ae0aade3626bb
Alina Walling <alina.walling@gmail.com>
2019-09-14 17:23:50 -0400

    Initial Commit

---
+++ GWDProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GWDProject
+//
+//  Created by Alina Walling on 9/14/19.
+//  Copyright © 2019 Alina Walling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
